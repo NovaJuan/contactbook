e59370b4b282a0a78a8c840548e68be94787936a
imByzon <juanrm.works07@gmail.com>
2019-11-28 18:36:32 -0400

    solving 404 problem

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ const cors = require('cors');
 const sanitize = require('express-mongo-sanitize');
 const limiter = require('express-rate-limit');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 connectDB();
 
 //Middlewares
-app.use(limiter({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100 // limit each IP to 100 requests per windowMs
-}));
+app.use(
+	limiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100 // limit each IP to 100 requests per windowMs
+	})
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(
@@ -31,6 +34,16 @@ app.use(cors());
 app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/contacts', require('./routes/contacts'));
+
+// Serve statics on production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server on port ${PORT}`));
